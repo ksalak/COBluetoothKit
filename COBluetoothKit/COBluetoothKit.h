d95477c9172f8bf9817a58303e7bac2812234e9e
Konstantin Salak <konstantin.salak@gmail.com>
2015-03-27 02:35:42 +0300

    Initial Commit

---
+++ COBluetoothKit/COBluetoothKit.h
@@ -0,0 +1,19 @@
+//
+//  COBluetoothKit.h
+//  COBluetoothKit
+//
+//  Created by Konstantin Salak on 3/27/15.
+//  Copyright (c) 2015 CocoaHeads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for COBluetoothKit.
+FOUNDATION_EXPORT double COBluetoothKitVersionNumber;
+
+//! Project version string for COBluetoothKit.
+FOUNDATION_EXPORT const unsigned char COBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <COBluetoothKit/PublicHeader.h>
+
+
